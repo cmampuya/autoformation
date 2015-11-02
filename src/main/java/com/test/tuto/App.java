f48cb456639e70246b00d81239da6571cb18be96
cmampuya <chriscomampuya@yahoo.fr>
2015-11-02 08:31:39 +0100

    fiveth commit

--- src/main/java/com/test/tuto/App.java
+++ src/main/java/com/test/tuto/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Chrisco, you are doing well and you win! ok" );
+        System.out.println( "Hello Chrisco mampuya, you are doing well and you win! ok" );
     }
 }
